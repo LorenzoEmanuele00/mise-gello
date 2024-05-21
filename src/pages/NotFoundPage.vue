8382ac729024fded7f710b841e16a32b58de4e20
Lorenzo Emanuele <140862390+LorenzoEmanuele00@users.noreply.github.com>
2024-05-21 22:54:34 +0200

    Initial commit

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1 class="text-center pt-5">404 Not Found</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: calc(100vh - 258px);
+  width: 100%;
+  overflow-y: auto
+}
+</style>
